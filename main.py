a5b554060bf70917fd2780b02541b1b372f99be7
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-02-24 00:09:57 +0200

    work parsel: 1. title phone 2.price 3. link by phone 4.link by site

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import requests
 import csv
 from bs4 import BeautifulSoup
-from config import URL, HEADER
+from config import URL, HEADER, MAIN_URL
+import re
 
 
 def get_html(url, params=""):
@@ -12,19 +13,32 @@ def get_html(url, params=""):
 def get_content(html):
     soup = BeautifulSoup(html.text, "html.parser")
     items = soup.find_all("div", class_="products-list-item products-catalog-grid__item products-list-item--grid")
-    cards_phones = []
+    prices = []
     for item in items:
-        cards_phones.append(
+        prices.append(item.find("div", {"class" : "products-list-item__actions-price-current"}))
+    clear_prices = []
+    for s in list(map(str, prices)):
+        digits_price = re.findall(r"\ (\d+\ \d+)", s)
+        if digits_price:
+            clear_prices.append(digits_price[0])
+        else:
+            clear_prices.append("Очікується")
+    cards = []
+    for s, item in zip(clear_prices, items):
+        cards.append(
             {
                 "model": item.find("div", class_="products-list-item__info").find("a").get_text(strip=True),
-                #"price": item.find("div", class_="products-list-item__actions-price-current").get_text(strip=True),       # не працює
-                "link_by_model": item.find("div", class_="products-list-item__info").find("a").get("href")
+                "price": s ,
+                "link_by_model": item.find("div", class_="products-list-item__info").find("a").get("href"),
+                "link_by_site" : MAIN_URL
             }
         )
 
+    return cards
 
-    return cards_phones
 
 
-# print(get_html(URL).text)
+
 print(get_content(get_html(URL)))
+
+
